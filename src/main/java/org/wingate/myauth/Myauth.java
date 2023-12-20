bcd7655df0b0066713bf432c181f6eb474299433
Yves LUDOSKY <util2@msn.com>
2023-12-20 17:30:33 +0100

    New project upload

---
+++ src/main/java/org/wingate/myauth/Myauth.java
@@ -0,0 +1,12 @@
+package org.wingate.myauth;
+
+/**
+ *
+ * @author util2
+ */
+public class Myauth {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
